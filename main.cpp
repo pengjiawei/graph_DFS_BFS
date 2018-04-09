0724cddb63836433c6cb9006d3f4e57f219e5727
pengjiawei <917527280@qq.com>
2018-04-09 14:39:45 +0800

    add DFS no recurse

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 
 typedef char vertexType;
 typedef int edgeType;
@@ -8,31 +9,70 @@ typedef int edgeType;
 typedef struct {
     vertexType vexs[MAXVEX];    //顶点表
     edgeType arc[MAXVEX][MAXVEX];   //邻接矩阵，也可以采用邻接表的方式来表示图
-    int numVertex,numEdges;
-}Graph;
+    int numVertex, numEdges;
+} Graph;
 bool visited[MAXVEX];
+
+int getNum(char c) {
+    if (c == 'a')
+        return 0;
+    if (c == 'b')
+        return 1;
+    if (c == 'c')
+        return 2;
+    if (c == 'd')
+        return 3;
+    if (c == 'e')
+        return 4;
+
+}
+
 //递归实现的深度优先遍历
-void DFS(Graph& g,int i){
+void DFS(Graph &g, int i) {
     visited[i] = true;
-    printf("vertex : %c\n",g.vexs[i]);
+    printf("vertex : %c\n", g.vexs[i]);
     for (int j = 0; j < g.numVertex; ++j) {
-        if (g.arc[i][j] == 1 && !visited[j]){
-            DFS(g,j);
+        if (g.arc[i][j] == 1 && !visited[j]) {
+            DFS(g, j);
         }
     }
 }
-void DFSTraverse(Graph g){
+
+void DFSTraverse(Graph g) {
     for (int i = 0; i < g.numVertex; ++i) {
         visited[i] = false;
     }
     for (int j = 0; j < g.numVertex; ++j) {
-        if (!visited[j]){
-            DFS(g,j);
+        if (!visited[j]) {
+            DFS(g, j);
         }
     }
 }
+
+void DFS_NotR(Graph g) {
+    std::stack<vertexType> s1;
+    for (int i = 0; i < g.numVertex; ++i) {
+        visited[i] = false;
+    }
+    int j = 0;
+    s1.push(g.vexs[j]);
+    visited[j] = true;
+
+    while (!s1.empty()) {
+        vertexType v1 = s1.top();
+        printf("vertex: %c\n", v1);
+        s1.pop();
+        for (int i = 0; i < g.numVertex; ++i) {
+            //寻找current node的邻节点
+            if (g.arc[getNum(v1)][i] == 1 && !visited[i]) {
+                s1.push(g.vexs[i]);
+            }
+        }
+    }
+}
+
 //广度优先照我的理解有点像层次遍历
-void BFSTraverse(Graph g){
+void BFSTraverse(Graph g) {
     std::queue<vertexType> q1;
     for (int i = 0; i < g.numVertex; ++i) {
         visited[i] = false;
@@ -41,12 +81,12 @@ void BFSTraverse(Graph g){
         if (!visited[i]) {
             visited[i] = true;
             q1.push(g.vexs[i]);
-            while(!q1.empty()){
+            while (!q1.empty()) {
                 vertexType v = q1.front();
-                printf("vertex : %c\n",v);
+                printf("vertex : %c\n", v);
                 q1.pop();
                 for (int j = 0; j < g.numVertex; ++j) {
-                    if(g.arc[i][j] == 1 && !visited[j]){
+                    if (g.arc[i][j] == 1 && !visited[j]) {
                         visited[j] = true;
                         q1.push(g.vexs[j]);
                     }
@@ -55,6 +95,7 @@ void BFSTraverse(Graph g){
         }
     }
 }
+
 int main() {
     Graph g;
     g.numVertex = 5;
@@ -68,8 +109,11 @@ int main() {
     g.arc[1][2] = 1;
     g.arc[0][3] = 1;
     g.arc[2][4] = 1;
-//    DFSTraverse(g);
+    DFSTraverse(g);
+    printf("\n");
     BFSTraverse(g);
+    printf("\n");
+    DFS_NotR(g);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
