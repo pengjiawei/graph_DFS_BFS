860019b4f5540875eaf01a1a6fd8145ab212b07d
pengjiawei <917527280@qq.com>
2018-04-09 13:53:52 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <queue>
+
+typedef char vertexType;
+typedef int edgeType;
+#define MAXEDGE 15
+#define MAXVEX 9
+typedef struct {
+    vertexType vexs[MAXVEX];    //顶点表
+    edgeType arc[MAXVEX][MAXVEX];   //邻接矩阵，也可以采用邻接表的方式来表示图
+    int numVertex,numEdges;
+}Graph;
+bool visited[MAXVEX];
+//递归实现的深度优先遍历
+void DFS(Graph& g,int i){
+    visited[i] = true;
+    printf("vertex : %c\n",g.vexs[i]);
+    for (int j = 0; j < g.numVertex; ++j) {
+        if (g.arc[i][j] == 1 && !visited[j]){
+            DFS(g,j);
+        }
+    }
+}
+void DFSTraverse(Graph g){
+    for (int i = 0; i < g.numVertex; ++i) {
+        visited[i] = false;
+    }
+    for (int j = 0; j < g.numVertex; ++j) {
+        if (!visited[j]){
+            DFS(g,j);
+        }
+    }
+}
+//广度优先照我的理解有点像层次遍历
+void BFSTraverse(Graph g){
+    std::queue<vertexType> q1;
+    for (int i = 0; i < g.numVertex; ++i) {
+        visited[i] = false;
+    }
+    for (int i = 0; i < g.numVertex; ++i) {
+        if (!visited[i]) {
+            visited[i] = true;
+            q1.push(g.vexs[i]);
+            while(!q1.empty()){
+                vertexType v = q1.front();
+                printf("vertex : %c\n",v);
+                q1.pop();
+                for (int j = 0; j < g.numVertex; ++j) {
+                    if(g.arc[i][j] == 1 && !visited[j]){
+                        visited[j] = true;
+                        q1.push(g.vexs[j]);
+                    }
+                }
+            }
+        }
+    }
+}
+int main() {
+    Graph g;
+    g.numVertex = 5;
+    g.numEdges = 4;
+    g.vexs[0] = 'a';
+    g.vexs[1] = 'b';
+    g.vexs[2] = 'c';
+    g.vexs[3] = 'd';
+    g.vexs[4] = 'e';
+    g.arc[0][1] = 1;
+    g.arc[1][2] = 1;
+    g.arc[0][3] = 1;
+    g.arc[2][4] = 1;
+//    DFSTraverse(g);
+    BFSTraverse(g);
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
